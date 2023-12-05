ccf2a30a2dd4d0126a8f4d54de5e6a0fcf7a4741
luke-tang <luke.tangh@outlook.com>
2023-12-05 11:36:04 +0000

    solution 1688

--- src/2264.py
+++ src/2264.py
@@ -1,7 +1,7 @@
 class Solution:
     def largestGoodInteger(self, num: str) -> str:
-        tris = ["999", "888", "777", "666", "555", "444", "333", "222", "111", "000"]
-        for n in tris:
-            if n in num:
-                return n
-        return ""
+        ret = ""
+        for i in range(2, len(num)):
+            if num[i - 2] == num[i - 1] == num[i]:
+                ret = max(ret, num[i]*3)
+        return ret
